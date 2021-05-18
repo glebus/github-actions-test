1bc8ff13fbec3a3a1e8a890a0589c9741f641bb1
Gleb Ustimenko <gleb.ustimenko@gmail.com>
2021-05-18 17:29:31 +0300

    test

---
+++ android/app/src/main/kotlin/com/example/calclist/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.calclist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Future main() async {
+  runApp(MaterialApp(home: Text("Hello")));
+}
